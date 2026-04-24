dcf4f2d4d52ad38e6f424bd6f8990e1727307e57
code-rk-101 <meetritik.kr@gmail.com>
2026-04-24 17:26:42 +0530

    fix samesite cookies

--- backend/src/controllers/auth.Controller.js
+++ backend/src/controllers/auth.Controller.js
@@ -139,7 +139,7 @@ const loginUser = [
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+        sameSite: "none",
       });
       res.status(200).json({
         message: "User Loggedin sucessfully",
