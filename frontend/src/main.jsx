77d8c17395a9809052cac3b1aa346bb16cd3ab2d
code-rk-101 <meetritik.kr@gmail.com>
2026-04-24 16:00:57 +0530

    removed scss imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./style.scss"
 import "./index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./features/auth/auth.context.jsx";
